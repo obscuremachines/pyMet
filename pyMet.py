2f68fd3ee108c50b038925577c73643fc8fff5bc
Thomas Jones <obscuremachines@gmail.com>
2015-02-18 01:56:19 -0600

    Added user defined cycle range

--- pyMet.py
+++ pyMet.py
@@ -4,11 +4,14 @@ from threading import Timer
 #prompts user to enter the beats per minute
 x = int(raw_input("BPM:"))
 
+#prompts user to enter the amount of counts per cycle
+timeSignature = int(raw_input("How many counts?"))
+
 #calculates the count interval using the user's BPM
 metspeed = 60.0 / x 
 
 #cycles through the defined range
-counts = cycle(xrange(1, 5))
+counts = cycle(xrange(1, timeSignature + 1))
 
 #prints the counts in the range in order one at a time
 def visual():
